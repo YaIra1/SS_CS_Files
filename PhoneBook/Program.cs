e70cc8f5871f746fa9676f5b7261f48422d44278
Iraya <irahotaru@gmail.com>
2023-07-27 20:29:25 +0300

    Implemented PhoneBook task, Files task

---
+++ PhoneBook/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new Dictionary<string, string>();
+            string phoneBookPath = "D:\\Phone_Book.txt";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(phoneBookPath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] tokens = line.Split(" ");
+                        phoneBook.Add(tokens[0], tokens[1]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            string phonesPath = "D:\\Phones.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(phonesPath))
+                {
+                    foreach (string phoneNumber in phoneBook.Values)
+                    {
+                        sw.WriteLine(phoneNumber);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Enter name");
+
+            string name = Console.ReadLine();
+
+            if (phoneBook.ContainsKey(name))
+            {
+                Console.WriteLine(phoneBook[name]);
+            }
+
+            string newPhonesPath = "D:\\New.txt";
+
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(newPhonesPath))
+                {
+                    foreach (var kvp in phoneBook)
+                    {
+                        string newNumber = kvp.Value;
+
+                        if (newNumber.StartsWith("80"))
+                        {
+                            newNumber = "+3" + newNumber;
+                        }
+                        streamWriter.WriteLine($"{kvp.Key} {newNumber}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SS_CS_Files/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace SS_CS_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task7_1_1();
+            // Task7_1_2();
+            // Task7_2();
+            Task7_3();
+
+        }
+
+        public static void Task7_1_1()
+        {
+            string sourcePath = "d:\\Repos\\SS_CS_Files\\data.txt";
+            string destinationPath = "d:\\Repos\\SS_CS_Files\\rez.txt";
+
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("The file does not exist");
+                return;
+            }
+
+            string destFolder = Path.GetDirectoryName(sourcePath);
+
+            if (!Directory.Exists(destFolder))
+            {
+                Console.WriteLine("The destination folder does not exist");
+                return;
+            }
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(sourcePath))
+                {
+                    string text = reader.ReadToEnd();
+
+                    using (StreamWriter writer = new StreamWriter(destinationPath, false))
+                    {
+                        writer.Write(text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void Task7_1_2()
+        {
+            string sourcePath = "d:\\Repos\\SS_CS_Files\\data.txt";
+            string destinationPath = "d:\\Repos\\SS_CS_Files\\rez.txt";
+
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("The file does not exist");
+                return;
+            }
+
+            string destFolder = Path.GetDirectoryName(sourcePath);
+
+            if (!Directory.Exists(destFolder))
+            {
+                Console.WriteLine("The destination folder does not exist");
+                return;
+            }
+
+            try
+            {
+                string text = File.ReadAllText(sourcePath);
+
+                File.WriteAllText(destinationPath, text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public static void Task7_2()
+        {
+            string dRoot = "D:\\";
+            string destination = "D:\\DriveD.txt";
+
+            try
+            {
+                string[] entries = Directory.GetFileSystemEntries(dRoot);
+
+                using (StreamWriter writer = new StreamWriter(destination))
+                {
+                    foreach (string entry in entries)
+                    {
+                        if (File.Exists(entry))
+                        {
+                            FileInfo info = new FileInfo(entry);
+
+                            if (info.Attributes.HasFlag(FileAttributes.Hidden))
+                            {
+                                continue;
+                            }
+                            writer.WriteLine($"File {info.Name} has extention: {info.Extension}, size: {info.Length}B");
+                        }
+                        else if (Directory.Exists(entry))
+                        {
+                            DirectoryInfo info = new DirectoryInfo(entry);
+
+                            if (info.Attributes.HasFlag(FileAttributes.Hidden))
+                            {
+                                continue;
+                            }
+                            writer.WriteLine($"Directory {info.Name}, size: {DirSize(info)}B");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public static long DirSize(DirectoryInfo dir)
+        {
+            return dir.GetFiles().Sum(fi => fi.Length) +
+                   dir.GetDirectories().Sum(di => DirSize(di));
+        }
+
+        public static void Task7_3()
+        {
+            string dRoot = "D:\\";
+            try
+            {
+                string[] files = Directory.GetFiles(dRoot, "*.txt");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(File.ReadAllText(file));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
